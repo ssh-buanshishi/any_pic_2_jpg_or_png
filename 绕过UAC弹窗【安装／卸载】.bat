f0d70b6c65fe39597a8419ae2493bc98ebc4dc58
ssh-buanshishi <128020866+ssh-buanshishi@users.noreply.github.com>
2025-08-24 22:27:05 +0800

    Add files via upload
    更新v1.13

---
+++ 绕过UAC弹窗【安装／卸载】.bat
@@ -0,0 +1,7 @@
+@echo off
+rem -------------------- encoding:GB 2312(ANSI) ---------------------
+rem It is recommended to use "Visual Studio Code" to edit this file.
+rem 写一点中文，防止记事本抽风
+
+msg "%username%" /TIME:30 "新版本不再需要管理员权限，所以无需安装，退出后可以删除本脚本"
+exit 0
